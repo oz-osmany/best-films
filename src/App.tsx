d934b67efaf4ffd4b0daa4e5bb686592d76bca07
Osmany Machado <osmachado1981@gmail.com>
2025-08-08 11:05:28 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <button className="bg-black text-white px-6 py-2 rounded-md hover:bg-gray-800">Hello shadcn/ui</button>
+    </div>
+  );
+}
